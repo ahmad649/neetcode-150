e7d8088979541e76610e75086da5598b98850305
Ahmad Hasan <58470792+ahmad649@users.noreply.github.com>
2026-08-07 16:36:21 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        counter1 = Counter(s)
+        counter2 = Counter(t)
+
+        if counter1 == counter2:
+            return True
+        else: 
+            return False
+
+
+        
